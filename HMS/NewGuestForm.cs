4c4087d79781f4965ef72b3f9e2ba28b595677ec
Marius Norheim <xeroxmf@gmail.com>
2018-03-20 20:23:18 +0100

    Added NewGuestForm class

---
+++ HMS/NewGuestForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HMS
+{
+    public partial class NewGuestForm : HMS.PopupForm
+    {
+        public NewGuestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonNewGuestConfirm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonNewGuestCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
