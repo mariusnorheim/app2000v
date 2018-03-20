8af0edef5059cdcbd5ebbc35eafa965beedfe8fc
Marius Norheim <xeroxmf@gmail.com>
2018-03-20 20:23:39 +0100

    Updated buttons and added load/click events

--- HMS/Sjekkinnut.Designer.cs
+++ HMS/Sjekkinnut.Designer.cs
@@ -33,9 +33,11 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.buttonSubMenu1 = new System.Windows.Forms.Button();
             this.buttonSubMenu2 = new System.Windows.Forms.Button();
-            this.buttonEndre = new System.Windows.Forms.Button();
-            this.buttonNew = new System.Windows.Forms.Button();
+            this.buttonNewCheckin = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonNewCheckout = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,6 +53,7 @@
             this.buttonSubMenu1.TabIndex = 39;
             this.buttonSubMenu1.Text = "Sjekk inn";
             this.buttonSubMenu1.UseVisualStyleBackColor = false;
+            this.buttonSubMenu1.Click += new System.EventHandler(this.buttonSubMenu1_Click);
             // 
             // buttonSubMenu2
             // 
@@ -64,40 +67,59 @@
             this.buttonSubMenu2.TabIndex = 40;
             this.buttonSubMenu2.Text = "Sjekk ut";
             this.buttonSubMenu2.UseVisualStyleBackColor = false;
+            this.buttonSubMenu2.Click += new System.EventHandler(this.buttonSubMenu2_Click);
             // 
-            // buttonEndre
+            // buttonNewCheckin
             // 
-            this.buttonEndre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
-            this.buttonEndre.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.buttonEndre.FlatAppearance.BorderSize = 0;
-            this.buttonEndre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonEndre.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonEndre.Location = new System.Drawing.Point(110, 355);
-            this.buttonEndre.Name = "buttonEndre";
-            this.buttonEndre.Size = new System.Drawing.Size(90, 32);
-            this.buttonEndre.TabIndex = 43;
-            this.buttonEndre.Text = "Endre";
-            this.buttonEndre.UseVisualStyleBackColor = false;
+            this.buttonNewCheckin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonNewCheckin.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.buttonNewCheckin.FlatAppearance.BorderSize = 0;
+            this.buttonNewCheckin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNewCheckin.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNewCheckin.Location = new System.Drawing.Point(15, 355);
+            this.buttonNewCheckin.Name = "buttonNewCheckin";
+            this.buttonNewCheckin.Size = new System.Drawing.Size(90, 32);
+            this.buttonNewCheckin.TabIndex = 42;
+            this.buttonNewCheckin.Text = "Sjekk inn";
+            this.buttonNewCheckin.UseVisualStyleBackColor = false;
+            this.buttonNewCheckin.Click += new System.EventHandler(this.buttonNewCheckin_Click);
             // 
-            // buttonNew
+            // textBoxSearch
             // 
-            this.buttonNew.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
-            this.buttonNew.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.buttonNew.FlatAppearance.BorderSize = 0;
-            this.buttonNew.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonNew.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonNew.Location = new System.Drawing.Point(15, 355);
-            this.buttonNew.Name = "buttonNew";
-            this.buttonNew.Size = new System.Drawing.Size(90, 32);
-            this.buttonNew.TabIndex = 42;
-            this.buttonNew.Text = "Ny";
-            this.buttonNew.UseVisualStyleBackColor = false;
+            this.textBoxSearch.AcceptsTab = true;
+            this.textBoxSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.textBoxSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxSearch.Font = new System.Drawing.Font("Bahnschrift Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSearch.ForeColor = System.Drawing.Color.White;
+            this.textBoxSearch.Location = new System.Drawing.Point(530, 356);
+            this.textBoxSearch.Margin = new System.Windows.Forms.Padding(0);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.ShortcutsEnabled = false;
+            this.textBoxSearch.Size = new System.Drawing.Size(300, 30);
+            this.textBoxSearch.TabIndex = 46;
+            this.textBoxSearch.Text = "Søketekst";
+            this.textBoxSearch.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBoxSearch_MouseClick);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonSearch.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.buttonSearch.FlatAppearance.BorderSize = 0;
+            this.buttonSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearch.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSearch.Location = new System.Drawing.Point(835, 355);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(50, 32);
+            this.buttonSearch.TabIndex = 45;
+            this.buttonSearch.Text = "Søk";
+            this.buttonSearch.UseVisualStyleBackColor = false;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.AllowUserToResizeColumns = false;
-            this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
@@ -130,33 +152,57 @@
             this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
             this.dataGridView1.Size = new System.Drawing.Size(870, 300);
-            this.dataGridView1.TabIndex = 44;
+            this.dataGridView1.TabIndex = 47;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // buttonNewCheckout
+            // 
+            this.buttonNewCheckout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonNewCheckout.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.buttonNewCheckout.FlatAppearance.BorderSize = 0;
+            this.buttonNewCheckout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNewCheckout.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNewCheckout.Location = new System.Drawing.Point(15, 355);
+            this.buttonNewCheckout.Name = "buttonNewCheckout";
+            this.buttonNewCheckout.Size = new System.Drawing.Size(90, 32);
+            this.buttonNewCheckout.TabIndex = 48;
+            this.buttonNewCheckout.Text = "Sjekk ut";
+            this.buttonNewCheckout.UseVisualStyleBackColor = false;
+            this.buttonNewCheckout.Click += new System.EventHandler(this.buttonNewCheckout_Click);
             // 
             // Sjekkinnut
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.ClientSize = new System.Drawing.Size(900, 620);
+            this.Controls.Add(this.buttonNewCheckout);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.buttonEndre);
-            this.Controls.Add(this.buttonNew);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonNewCheckin);
             this.Controls.Add(this.buttonSubMenu2);
             this.Controls.Add(this.buttonSubMenu1);
             this.Name = "Sjekkinnut";
+            this.Load += new System.EventHandler(this.Sjekkinnut_Load);
             this.Controls.SetChildIndex(this.buttonSubMenu1, 0);
             this.Controls.SetChildIndex(this.buttonSubMenu2, 0);
-            this.Controls.SetChildIndex(this.buttonNew, 0);
-            this.Controls.SetChildIndex(this.buttonEndre, 0);
+            this.Controls.SetChildIndex(this.buttonNewCheckin, 0);
+            this.Controls.SetChildIndex(this.buttonSearch, 0);
+            this.Controls.SetChildIndex(this.textBoxSearch, 0);
             this.Controls.SetChildIndex(this.dataGridView1, 0);
+            this.Controls.SetChildIndex(this.buttonNewCheckout, 0);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button buttonSubMenu1;
         private System.Windows.Forms.Button buttonSubMenu2;
-        private System.Windows.Forms.Button buttonEndre;
-        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.Button buttonNewCheckin;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonSearch;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonNewCheckout;
     }
 }
--- HMS/Sjekkinnut.cs
+++ HMS/Sjekkinnut.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace HMS
 {
@@ -13,6 +14,195 @@ namespace HMS
         public Sjekkinnut()
         {
             InitializeComponent();
+        }
+
+        private void Sjekkinnut_Load(object sender, System.EventArgs e)
+        {
+            //ThreadPool.QueueUserWorkItem(LoadData);
+            LoadDataSjekkinn();
+        }
+        string query;
+
+        //private void LoadData(Object threadContext)
+        private void LoadDataSjekkinn()
+        {
+            this.buttonNewCheckout.Visible = false;
+            this.buttonNewCheckin.Visible = true;
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(DBConn.ConnectionString);
+                // Connect to database
+                conn.Open();
+
+                // Create a new data adapter
+                query = "SELECT RR.room_reservationid, G.guest_name, RT.room_type_name, " +
+                    "RR.room_reservation_amount, RR.room_reservation_datefrom, RR.room_reservation_dateto, " +
+                    "RR.room_reservation_remark FROM room_reservation RR " +
+                    "JOIN room_type RT " +
+                    "ON RR.room_reservation_typeid = RT.room_typeid " +
+                    "JOIN guest G " +
+                    "ON RR.room_reservation_guestid = G.guestid " +
+                    "ORDER BY room_reservation_datefrom, guest_name";
+                MySqlDataAdapter bookingTableAdapter = new MySqlDataAdapter(query, conn);
+                // Populate a new data set
+                DataSet bookingDS = new DataSet();
+                bookingTableAdapter.Fill(bookingDS, "room_reservation");
+                dataGridView1.DataSource = bookingDS;
+                dataGridView1.DataMember = "room_reservation";
+                // Hide ID and make readable table headers
+                dataGridView1.Columns[0].Visible = false;
+                dataGridView1.Columns[1].HeaderText = "Gjest";
+                dataGridView1.Columns[2].HeaderText = "Romtype";
+                dataGridView1.Columns[3].HeaderText = "Romantall";
+                dataGridView1.Columns[4].HeaderText = "Ankomst";
+                dataGridView1.Columns[5].HeaderText = "Avreise";
+                dataGridView1.Columns[6].HeaderText = "Anmerkning";
+
+                // Close database connection
+                conn.Close();
+            }
+            // Catch exceptions and display in labelStatus
+            catch (Exception ex)
+            {
+                this.labelStatus.Text = ex.Message;
+            }
+        }
+
+        private void LoadDataSjekkut()
+        {
+            this.buttonNewCheckin.Visible = false;
+            this.buttonNewCheckout.Visible = true;
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(DBConn.ConnectionString);
+                // Connect to database
+                conn.Open();
+
+                // Create a new data adapter
+                query = "SELECT RR.room_reservationid, G.guest_name, RT.room_type_name, " +
+                    "RR.room_reservation_amount, RR.room_reservation_datefrom, RR.room_reservation_dateto, " +
+                    "RR.room_reservation_remark FROM room_reservation RR " +
+                    "JOIN room_type RT " +
+                    "ON RR.room_reservation_typeid = RT.room_typeid " +
+                    "JOIN guest G " +
+                    "ON RR.room_reservation_guestid = G.guestid " +
+                    "ORDER BY room_reservation_dateto, guest_name";
+                MySqlDataAdapter bookingTableAdapter = new MySqlDataAdapter(query, conn);
+                // Populate a new data set
+                DataSet bookingDS = new DataSet();
+                bookingTableAdapter.Fill(bookingDS, "room_reservation");
+                dataGridView1.DataSource = bookingDS;
+                dataGridView1.DataMember = "room_reservation";
+                // Hide ID and make readable table headers
+                dataGridView1.Columns[0].Visible = false;
+                dataGridView1.Columns[1].HeaderText = "Gjest";
+                dataGridView1.Columns[2].HeaderText = "Romtype";
+                dataGridView1.Columns[3].HeaderText = "Romantall";
+                dataGridView1.Columns[4].HeaderText = "Ankomst";
+                dataGridView1.Columns[5].HeaderText = "Avreise";
+                dataGridView1.Columns[6].HeaderText = "Anmerkning";
+
+                // Close database connection
+                conn.Close();
+            }
+            // Catch exceptions and display in labelStatus
+            catch (Exception ex)
+            {
+                this.labelStatus.Text = ex.Message;
+            }
+        }
+
+        private void buttonSubMenu1_Click(object sender, EventArgs e)
+        {
+            LoadDataSjekkinn();
+        }
+
+        private void buttonSubMenu2_Click(object sender, EventArgs e)
+        {
+            LoadDataSjekkut();
+        }
+
+        // DataGridView double click, open edit form
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Set database record ID for reference
+            DBConn.QueryID = this.dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            // New checkin form
+            Form editForm = new Form();
+            editForm.ShowDialog();
+        }
+
+        // Button 'Sjekk inn', identical to datagridview double click
+        private void buttonNewCheckin_Click(object sender, EventArgs e)
+        {
+            // Set database record ID for reference
+            DBConn.QueryID = this.dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            // New checkin form
+            Form editForm = new Form();
+            editForm.ShowDialog();
+        }
+
+        // Button 'Sjekk inn', identical to datagridview double click
+        private void buttonNewCheckout_Click(object sender, EventArgs e)
+        {
+            // Set database record ID for reference
+            DBConn.QueryID = this.dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            // New checkout form
+            Form editForm = new Form();
+            editForm.ShowDialog();
+        }
+
+        // TextBox 'Search' click event, empty placeholder text
+        private void textBoxSearch_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.textBoxSearch.Text = "";
+        }
+
+        // Button 'Søk', searches result in relevant tables from textinput
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string search_input = @textBoxSearch.Text.Trim();
+                MySqlConnection conn = new MySqlConnection(DBConn.ConnectionString);
+                // Connect to database
+                conn.Open();
+
+                // Create a new data adapter
+                query = "SELECT RR.room_reservationid, G.guest_name, RT.room_type_name, " +
+                    "RR.room_reservation_amount, RR.room_reservation_datefrom, RR.room_reservation_dateto, " +
+                    "RR.room_reservation_remark FROM room_reservation RR " +
+                    "JOIN room_type RT " +
+                    "ON RR.room_reservation_typeid = RT.room_typeid " +
+                    "JOIN guest G " +
+                    "ON RR.room_reservation_guestid = G.guestid " +
+                    "WHERE guest_name LIKE @search " +
+                    "ORDER BY room_reservation_datefrom, guest_name";
+                MySqlCommand cmdGetSearch = new MySqlCommand(query, conn);
+                cmdGetSearch.Parameters.AddWithValue("@search", "%" + search_input + "%");
+                MySqlDataAdapter bookingSearchTableAdapter = new MySqlDataAdapter(cmdGetSearch);
+                // Populate a new data set
+                DataSet checkinSearchDS = new DataSet();
+                bookingSearchTableAdapter.Fill(checkinSearchDS, "room_reservation");
+                dataGridView1.DataSource = checkinSearchDS;
+                dataGridView1.DataMember = "room_reservation";
+                // Hide ID and make readable table headers
+                dataGridView1.Columns[0].Visible = false;
+                dataGridView1.Columns[1].HeaderText = "Gjest";
+                dataGridView1.Columns[2].HeaderText = "Romtype";
+                dataGridView1.Columns[3].HeaderText = "Romantall";
+                dataGridView1.Columns[4].HeaderText = "Ankomst";
+                dataGridView1.Columns[5].HeaderText = "Avreise";
+                dataGridView1.Columns[6].HeaderText = "Anmerkning";
+
+                // Close database connection
+                conn.Close();
+            }
+            // Catch exceptions and display in labelStatus
+            catch (Exception ex)
+            {
+                this.labelStatus.Text = ex.Message;
+            }
         }
     }
 }
