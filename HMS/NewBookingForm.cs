fa78b357f4edcd92ef2183f98adbe8202a92457a
Marius Norheim <xeroxmf@gmail.com>
2018-03-20 17:27:28 +0100

    Added form for new booking

---
+++ HMS/NewBookingForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HMS
+{
+    public partial class NewBookingForm : HMS.PopupForm
+    {
+        public NewBookingForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
