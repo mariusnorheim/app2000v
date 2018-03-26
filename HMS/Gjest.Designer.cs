e85e28d456ebf61d5d753935d5e2ff94d050b93a
Marius Norheim <xeroxmf@gmail.com>
2018-03-27 01:36:32 +0200

    Added shell controls

---
+++ HMS/Gjest.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace HMS
+{
+    partial class Gjest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridViewGuest = new System.Windows.Forms.DataGridView();
+            this.buttonEditGuest = new System.Windows.Forms.Button();
+            this.buttonNewGuest = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonSearchGuest = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGuest)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewGuest
+            // 
+            this.dataGridViewGuest.AllowUserToAddRows = false;
+            this.dataGridViewGuest.AllowUserToDeleteRows = false;
+            this.dataGridViewGuest.AllowUserToResizeColumns = false;
+            this.dataGridViewGuest.AllowUserToResizeRows = false;
+            this.dataGridViewGuest.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewGuest.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.dataGridViewGuest.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewGuest.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewGuest.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewGuest.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewGuest.GridColor = System.Drawing.Color.DimGray;
+            this.dataGridViewGuest.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dataGridViewGuest.Location = new System.Drawing.Point(15, 50);
+            this.dataGridViewGuest.Name = "dataGridViewGuest";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewGuest.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridViewGuest.RowHeadersVisible = false;
+            this.dataGridViewGuest.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridViewGuest.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridViewGuest.Size = new System.Drawing.Size(970, 450);
+            this.dataGridViewGuest.TabIndex = 39;
+            // 
+            // buttonEditGuest
+            // 
+            this.buttonEditGuest.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonEditGuest.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonEditGuest.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEditGuest.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEditGuest.Location = new System.Drawing.Point(100, 505);
+            this.buttonEditGuest.Name = "buttonEditGuest";
+            this.buttonEditGuest.Size = new System.Drawing.Size(80, 30);
+            this.buttonEditGuest.TabIndex = 49;
+            this.buttonEditGuest.Text = "Endre";
+            this.buttonEditGuest.UseVisualStyleBackColor = false;
+            this.buttonEditGuest.Click += new System.EventHandler(this.buttonEditGuest_Click);
+            // 
+            // buttonNewGuest
+            // 
+            this.buttonNewGuest.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonNewGuest.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonNewGuest.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNewGuest.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNewGuest.Location = new System.Drawing.Point(15, 505);
+            this.buttonNewGuest.Name = "buttonNewGuest";
+            this.buttonNewGuest.Size = new System.Drawing.Size(80, 30);
+            this.buttonNewGuest.TabIndex = 48;
+            this.buttonNewGuest.Text = "Ny";
+            this.buttonNewGuest.UseVisualStyleBackColor = false;
+            this.buttonNewGuest.Click += new System.EventHandler(this.buttonNewGuest_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.AcceptsTab = true;
+            this.textBoxSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.textBoxSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxSearch.Font = new System.Drawing.Font("Bahnschrift Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSearch.ForeColor = System.Drawing.Color.White;
+            this.textBoxSearch.Location = new System.Drawing.Point(630, 505);
+            this.textBoxSearch.Margin = new System.Windows.Forms.Padding(0);
+            this.textBoxSearch.MaxLength = 80;
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(300, 30);
+            this.textBoxSearch.TabIndex = 50;
+            this.textBoxSearch.Text = "Søketekst";
+            this.textBoxSearch.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBoxSearch_MouseClick);
+            // 
+            // buttonSearchGuest
+            // 
+            this.buttonSearchGuest.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonSearchGuest.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonSearchGuest.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearchGuest.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSearchGuest.Location = new System.Drawing.Point(935, 505);
+            this.buttonSearchGuest.Name = "buttonSearchGuest";
+            this.buttonSearchGuest.Size = new System.Drawing.Size(50, 30);
+            this.buttonSearchGuest.TabIndex = 51;
+            this.buttonSearchGuest.Text = "Søk";
+            this.buttonSearchGuest.UseVisualStyleBackColor = false;
+            this.buttonSearchGuest.Click += new System.EventHandler(this.buttonSearchGuest_Click);
+            // 
+            // Gjest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(1000, 670);
+            this.Controls.Add(this.buttonSearchGuest);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.buttonEditGuest);
+            this.Controls.Add(this.buttonNewGuest);
+            this.Controls.Add(this.dataGridViewGuest);
+            this.Name = "Gjest";
+            this.Load += new System.EventHandler(this.Gjest_Load);
+            this.Controls.SetChildIndex(this.dataGridViewGuest, 0);
+            this.Controls.SetChildIndex(this.buttonNewGuest, 0);
+            this.Controls.SetChildIndex(this.buttonEditGuest, 0);
+            this.Controls.SetChildIndex(this.textBoxSearch, 0);
+            this.Controls.SetChildIndex(this.buttonSearchGuest, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGuest)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewGuest;
+        private System.Windows.Forms.Button buttonEditGuest;
+        private System.Windows.Forms.Button buttonNewGuest;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonSearchGuest;
+    }
+}
---
+++ HMS/Gjest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace HMS
+{
+    public partial class Gjest : HMS.Content
+    {
+        MySqlDataAdapter guestTableAdapter;
+        DataSet guestDS;
+        string query;
+
+        public Gjest()
+        {
+            InitializeComponent();
+        }
+
+        private void Gjest_Load(object sender, System.EventArgs e)
+        {
+            LoadDataGuest();
+        }
+
+        private void LoadDataGuest()
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(DBConn.ConnectionString);
+                // Connect to database
+                conn.Open();
+
+                // Populate a new data set to datagridview
+                query = "SELECT guestid, CONCAT(firstname, lastname) as guest_name,  " +
+                    "address, postcode, city, telephone, membersince FROM guest " +
+                    "ORDER BY lastname, firstname";
+                guestTableAdapter = new MySqlDataAdapter(query, conn);
+                MySqlCommandBuilder guestTableCmd = new MySqlCommandBuilder(guestTableAdapter);
+                guestDS = new DataSet();
+                guestTableAdapter.Fill(guestDS, "guest");
+                dataGridViewGuest.DataSource = guestDS;
+                dataGridViewGuest.DataMember = "guest";
+                // Hide ID and make readable table headers
+                dataGridViewGuest.Columns[0].Visible = false;
+                dataGridViewGuest.Columns[1].HeaderText = "Fornavn";
+                dataGridViewGuest.Columns[2].HeaderText = "Etternavn";
+                dataGridViewGuest.Columns[3].HeaderText = "Adresse";
+                dataGridViewGuest.Columns[4].HeaderText = "Postkode";
+                dataGridViewGuest.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                dataGridViewGuest.Columns[5].HeaderText = "By";
+                dataGridViewGuest.Columns[6].HeaderText = "Telefon";
+                dataGridViewGuest.Columns[6].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                //dataGridViewGuest.Columns[7].HeaderText = "Medlem siden";
+                //dataGridViewGuest.Columns[7].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+
+                // Close database connection
+                conn.Close();
+            }
+            // Catch exceptions and display in labelStatus
+            catch (Exception ex)
+            {
+                this.labelStatus.Text = ex.Message;
+            }
+        }
+
+        private void buttonNewGuest_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonEditGuest_Click(object sender, EventArgs e)
+        {
+            DialogResult confirmEdit = MessageBox.Show("Are you sure you want to edit the database table?\nClick \"No\" if you are unsure.", "Confirm", MessageBoxButtons.YesNo);
+            if (confirmEdit == DialogResult.Yes)
+            {
+                guestTableAdapter.Update(guestDS, "guest");
+                this.labelStatus.Text = "Databasen oppdatert.";
+            }
+            LoadDataGuest();
+        }
+
+        private void buttonSearchGuest_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // TextBox 'Search' click event, empty placeholder text
+        private void textBoxSearch_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.textBoxSearch.Text = "";
+        }
+    }
+}
