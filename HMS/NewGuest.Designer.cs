d2dad5919b4929bd88ffbba5cb2791abbd6eda51
Marius Norheim <xeroxmf@gmail.com>
2018-03-29 12:05:44 +0200

    Added new guest form

---
+++ HMS/NewGuest.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace HMS
+{
+    partial class NewGuest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFirstname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxLastname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxPostcode = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxTelephone = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonNewGuestConfirm = new System.Windows.Forms.Button();
+            this.buttonNewGuestCancel = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxFirstname
+            // 
+            this.textBoxFirstname.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxFirstname.Font = new System.Drawing.Font("Verdana", 9.75F);
+            this.textBoxFirstname.Location = new System.Drawing.Point(135, 15);
+            this.textBoxFirstname.MaxLength = 40;
+            this.textBoxFirstname.Name = "textBoxFirstname";
+            this.textBoxFirstname.Size = new System.Drawing.Size(225, 23);
+            this.textBoxFirstname.TabIndex = 53;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 52;
+            this.label1.Text = "Fornavn";
+            // 
+            // textBoxLastname
+            // 
+            this.textBoxLastname.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxLastname.Font = new System.Drawing.Font("Verdana", 9.75F);
+            this.textBoxLastname.Location = new System.Drawing.Point(135, 42);
+            this.textBoxLastname.MaxLength = 40;
+            this.textBoxLastname.Name = "textBoxLastname";
+            this.textBoxLastname.Size = new System.Drawing.Size(225, 23);
+            this.textBoxLastname.TabIndex = 55;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 54;
+            this.label2.Text = "Etternavn";
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxAddress.Font = new System.Drawing.Font("Verdana", 9.75F);
+            this.textBoxAddress.Location = new System.Drawing.Point(135, 69);
+            this.textBoxAddress.MaxLength = 100;
+            this.textBoxAddress.Multiline = true;
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(225, 54);
+            this.textBoxAddress.TabIndex = 57;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(65, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 56;
+            this.label3.Text = "Adresse";
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxCity.Font = new System.Drawing.Font("Verdana", 9.75F);
+            this.textBoxCity.Location = new System.Drawing.Point(135, 127);
+            this.textBoxCity.MaxLength = 45;
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(225, 23);
+            this.textBoxCity.TabIndex = 59;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(65, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 16);
+            this.label4.TabIndex = 58;
+            this.label4.Text = "By";
+            // 
+            // textBoxPostcode
+            // 
+            this.textBoxPostcode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxPostcode.Font = new System.Drawing.Font("Verdana", 9.75F);
+            this.textBoxPostcode.Location = new System.Drawing.Point(135, 154);
+            this.textBoxPostcode.MaxLength = 10;
+            this.textBoxPostcode.Name = "textBoxPostcode";
+            this.textBoxPostcode.Size = new System.Drawing.Size(225, 23);
+            this.textBoxPostcode.TabIndex = 61;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(65, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 16);
+            this.label5.TabIndex = 60;
+            this.label5.Text = "Postkode";
+            // 
+            // textBoxTelephone
+            // 
+            this.textBoxTelephone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxTelephone.Font = new System.Drawing.Font("Verdana", 9.75F);
+            this.textBoxTelephone.Location = new System.Drawing.Point(135, 181);
+            this.textBoxTelephone.MaxLength = 20;
+            this.textBoxTelephone.Name = "textBoxTelephone";
+            this.textBoxTelephone.Size = new System.Drawing.Size(225, 23);
+            this.textBoxTelephone.TabIndex = 63;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(65, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 16);
+            this.label6.TabIndex = 62;
+            this.label6.Text = "Telefon *";
+            // 
+            // buttonNewGuestConfirm
+            // 
+            this.buttonNewGuestConfirm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.buttonNewGuestConfirm.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonNewGuestConfirm.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonNewGuestConfirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNewGuestConfirm.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNewGuestConfirm.Location = new System.Drawing.Point(135, 209);
+            this.buttonNewGuestConfirm.Name = "buttonNewGuestConfirm";
+            this.buttonNewGuestConfirm.Size = new System.Drawing.Size(60, 28);
+            this.buttonNewGuestConfirm.TabIndex = 65;
+            this.buttonNewGuestConfirm.Text = "Lagre";
+            this.buttonNewGuestConfirm.UseVisualStyleBackColor = false;
+            this.buttonNewGuestConfirm.Click += new System.EventHandler(this.buttonNewGuestConfirm_Click);
+            // 
+            // buttonNewGuestCancel
+            // 
+            this.buttonNewGuestCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.buttonNewGuestCancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonNewGuestCancel.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonNewGuestCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNewGuestCancel.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNewGuestCancel.Location = new System.Drawing.Point(200, 209);
+            this.buttonNewGuestCancel.Name = "buttonNewGuestCancel";
+            this.buttonNewGuestCancel.Size = new System.Drawing.Size(60, 28);
+            this.buttonNewGuestCancel.TabIndex = 64;
+            this.buttonNewGuestCancel.Text = "Avbryt";
+            this.buttonNewGuestCancel.UseVisualStyleBackColor = false;
+            this.buttonNewGuestCancel.Click += new System.EventHandler(this.buttonNewGuestCancel_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(132, 240);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(231, 14);
+            this.label7.TabIndex = 81;
+            this.label7.Text = "Felt markert med * må ikke fylles ut";
+            // 
+            // NewGuest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(434, 261);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.buttonNewGuestConfirm);
+            this.Controls.Add(this.buttonNewGuestCancel);
+            this.Controls.Add(this.textBoxTelephone);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxPostcode);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxLastname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxFirstname);
+            this.Controls.Add(this.label1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "NewGuest";
+            this.Text = "Ny gjest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFirstname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxLastname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxPostcode;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxTelephone;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonNewGuestConfirm;
+        private System.Windows.Forms.Button buttonNewGuestCancel;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ HMS/NewGuest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace HMS
+{
+    public partial class NewGuest : HMS.PopupForm
+    {
+        Gjest guestForm = (Gjest)Application.OpenForms["Gjest"];
+        private string query;
+
+        public NewGuest()
+        {
+            InitializeComponent();
+        }
+
+        // Button 'Lagre'
+        // Insert record into database
+        private void buttonNewGuestConfirm_Click(object sender, EventArgs e)
+        {
+            string firstname = textBoxFirstname.Text;
+            string lastname = textBoxLastname.Text;
+            string address = textBoxAddress.Text;
+            string city = textBoxCity.Text;
+            string postcode = textBoxPostcode.Text;
+            string telephone = textBoxTelephone.Text;
+
+            // Check for null or empty input
+            if (string.IsNullOrWhiteSpace(firstname)) { MessageBox.Show("Fornavn ikke fylt ut eller kun tomrom"); }
+            if (string.IsNullOrWhiteSpace(lastname)) { MessageBox.Show("Etternavn ikke fylt ut eller kun tomrom"); }
+            if (string.IsNullOrWhiteSpace(address)) { MessageBox.Show("Adresse ikke fylt ut eller kun tomrom"); }
+            if (string.IsNullOrWhiteSpace(city)) { MessageBox.Show("By ikke fylt ut eller kun tomrom"); }
+            if (string.IsNullOrWhiteSpace(postcode)) { MessageBox.Show("Postkode ikke fylt ut eller kun tomrom"); }
+            if (string.IsNullOrWhiteSpace(telephone)) { telephone = null; }
+
+            if (!string.IsNullOrWhiteSpace(firstname) && !string.IsNullOrWhiteSpace(lastname) && !string.IsNullOrWhiteSpace(address)
+                && !string.IsNullOrWhiteSpace(city) && !string.IsNullOrWhiteSpace(postcode))
+            {
+                using (MySqlConnection conn = new MySqlConnection(DBConn.ConnectionString))
+                {
+                    try
+                    {
+                        // Connect to database
+                        conn.Open();
+                        // Save record to database
+                        query = "INSERT INTO guest (firstname, lastname, address, city, postcode, telephone) " +
+                                "VALUES (@firstname, @lastname, @address, @city, @postcode, @telephone)";
+                        using (MySqlCommand newGuestCmd = new MySqlCommand(query, conn))
+                        {
+                            newGuestCmd.Parameters.AddWithValue("@firstname", firstname);
+                            newGuestCmd.Parameters.AddWithValue("@lastname", lastname);
+                            newGuestCmd.Parameters.AddWithValue("@address", address);
+                            newGuestCmd.Parameters.AddWithValue("@city", city);
+                            newGuestCmd.Parameters.AddWithValue("@postcode", postcode);
+                            newGuestCmd.Parameters.AddWithValue("@telephone", telephone);
+                            newGuestCmd.ExecuteNonQuery();
+                            // Close form and refresh data
+                            this.Close();
+                            guestForm.LoadDataGuest();
+                            guestForm.labelStatus.Text = "Gjest med navn " + firstname + " " + lastname + " er lagt til i databasen.";
+                        }
+                    }
+                    // Catch exceptions and display in MessageBox
+                    catch (Exception ex) { MessageBox.Show(ex.Message); }
+                    // Make sure connection is closed
+                    finally
+                    {
+                        if (conn.State == System.Data.ConnectionState.Open) { conn.Close(); }
+                    }
+                }
+            }
+        }
+
+        // Button 'Avbryt'
+        private void buttonNewGuestCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            guestForm.LoadDataGuest();
+        }
+    }
+}
