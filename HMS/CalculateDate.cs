a254b15404861446cac950b9ed3c626f15215d94
Marius Norheim <xeroxmf@gmail.com>
2018-03-29 06:57:14 +0200

    Added class to calculate first day of week

---
+++ HMS/CalculateDate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+public static class CalculateDate
+{
+    // Returns the first day of the week that the specified
+    // date is in using the current culture. 
+    public static DateTime GetFirstDayOfWeek(DateTime dayInWeek)
+    {
+        CultureInfo defaultCultureInfo = CultureInfo.CurrentCulture;
+        return GetFirstDateOfWeek(dayInWeek, defaultCultureInfo);
+    }
+
+    // Returns the first date of the week that the specified date is in. 
+    public static DateTime GetFirstDateOfWeek(DateTime dayInWeek, CultureInfo cultureInfo)
+    {
+        DayOfWeek firstDay = cultureInfo.DateTimeFormat.FirstDayOfWeek;
+        DateTime firstDayInWeek = dayInWeek.Date;
+        while (firstDayInWeek.DayOfWeek != firstDay) { firstDayInWeek = firstDayInWeek.AddDays(-1); }
+
+        return firstDayInWeek;
+    }
+}
