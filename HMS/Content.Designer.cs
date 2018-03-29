b631078dc358c903753acaa07350f1e356f9827d
Marius Norheim <xeroxmf@gmail.com>
2018-03-29 06:57:37 +0200

    Changed font on labelStatus

--- HMS/Content.Designer.cs
+++ HMS/Content.Designer.cs
@@ -45,10 +45,10 @@
             // labelStatus
             // 
             this.labelStatus.AutoSize = true;
-            this.labelStatus.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelStatus.Location = new System.Drawing.Point(2, 2);
             this.labelStatus.Name = "labelStatus";
-            this.labelStatus.Size = new System.Drawing.Size(0, 15);
+            this.labelStatus.Size = new System.Drawing.Size(0, 16);
             this.labelStatus.TabIndex = 0;
             // 
             // Content
