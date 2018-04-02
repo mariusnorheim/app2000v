b802327c3eb9bfd4745eaf148b755aeb793e7591
Marius Norheim <xeroxmf@gmail.com>
2018-04-02 17:23:55 +0200

    Added classe to invoke new statusmessage

---
+++ HMS/StatusMessage.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace HMS
+{
+    /// <summary>
+    /// Displays status message in labelStatus
+    /// </summary>
+    public class StatusMessage
+    {
+        UserInterface uiForm = (UserInterface)Application.OpenForms["UserInterface"];
+
+        public StatusMessage(string msg)
+        {
+            uiForm.labelStatusMessage.Text = msg;
+        }
+    }
+}
