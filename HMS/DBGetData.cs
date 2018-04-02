09e81c20d1219a5c72a340f3aeffe3b1af0551ca
Marius Norheim <xeroxmf@gmail.com>
2018-04-02 17:23:31 +0200

    Added folio section

--- HMS/DBGetData.cs
+++ HMS/DBGetData.cs
@@ -6,144 +6,134 @@ using MySql.Data.MySqlClient;
 
 namespace HMS
 {
-    public class DBGetData
+    /// <summary>
+    /// Returns spesific data with DBconn objects
+    /// </summary>
+    public static class DBGetData
     {
+        // QueryID reference for edit forms
+        private static int queryID;
+        public static int QueryID
+        {
+            get => queryID;
+            set => queryID = value;
+        }
+
         //
         //  Booking data
         //
-        public DataSet GetRoomBookingsAll(int isActive)
+        public static DataSet GetRoomBookingsAll(int isActive)
         {
-            DBConn dbconn = new DBConn();
             List<DbParameter> parameters = new List<DbParameter>();
-            parameters.Add(new MySqlParameter("isActive", isActive));
+            parameters.Add(new MySqlParameter("active", isActive));
 
+            DBConn dbconn = new DBConn();
             return dbconn.GetDataSet("GetRoomReservations_All", parameters);
         }
 
-        public DataSet GetRoomBookingsBetweenDates(DateTime datefrom, DateTime dateto)
+        public static DataSet GetRoomBookingsBetweenDates(DateTime datefrom, DateTime dateto)
         {
-            DBConn dbconn = new DBConn();
             List<DbParameter> parameters = new List<DbParameter>();
-            parameters.Add(new MySqlParameter("dateFrom", datefrom));
-            parameters.Add(new MySqlParameter("dateTo", dateto));
+            parameters.Add(new MySqlParameter("df", datefrom));
+            parameters.Add(new MySqlParameter("dt", dateto));
 
+            DBConn dbconn = new DBConn();
             return dbconn.GetDataSet("GetRoomReservations_BetweenDates", parameters);
         }
 
-        public DataSet GetRoomBookingsSpesificDate(DateTime date)
+        public static DataSet GetRoomBookingsSpesificDate(DateTime date)
         {
-            DBConn dbconn = new DBConn();
             List<DbParameter> parameters = new List<DbParameter>();
-            parameters.Add(new MySqlParameter("dateSelected", date));
+            parameters.Add(new MySqlParameter("ds", date));
 
+            DBConn dbconn = new DBConn();
             return dbconn.GetDataSet("GetRoomReservations_SpesificDate", parameters);
         }
 
-        public DataSet GetHallBookingsAll(int isActive)
+        public static DataSet GetRoomBookingsSearch(string input)
         {
-            DBConn dbconn = new DBConn();
             List<DbParameter> parameters = new List<DbParameter>();
-            parameters.Add(new MySqlParameter("isActive", isActive));
+            parameters.Add(new MySqlParameter("Search", "%" + input + "%"));
 
+            DBConn dbconn = new DBConn();
+            return dbconn.GetDataSet("GetRoomReservations_Search", parameters);
+        }
+
+        public static DataSet GetHallBookingsAll(int isActive)
+        {
+            List<DbParameter> parameters = new List<DbParameter>();
+            parameters.Add(new MySqlParameter("active", isActive));
+
+            DBConn dbconn = new DBConn();
             return dbconn.GetDataSet("GetHallReservations_All", parameters);
         }
 
-        public DataSet GetHallBookingsBetweenDates(DateTime datefrom, DateTime dateto)
+        public static DataSet GetHallBookingsBetweenDates(DateTime datefrom, DateTime dateto)
         {
-            DBConn dbconn = new DBConn();
             List<DbParameter> parameters = new List<DbParameter>();
-            parameters.Add(new MySqlParameter("dateFrom", datefrom));
-            parameters.Add(new MySqlParameter("dateTo", dateto));
+            parameters.Add(new MySqlParameter("df", datefrom));
+            parameters.Add(new MySqlParameter("dt", dateto));
 
+            DBConn dbconn = new DBConn();
             return dbconn.GetDataSet("GetHallReservations_BetweenDates", parameters);
         }
 
-        public DataSet GetHallBookingsSpesificDate(DateTime date)
+        public static DataSet GetHallBookingsSpesificDate(DateTime date)
         {
-            DBConn dbconn = new DBConn();
             List<DbParameter> parameters = new List<DbParameter>();
-            parameters.Add(new MySqlParameter("dateSelected", date));
+            parameters.Add(new MySqlParameter("ds", date));
 
+            DBConn dbconn = new DBConn();
             return dbconn.GetDataSet("GetHallReservations_SpesificDate", parameters);
         }
+
 
         //
         // Guest data
         //
-        public DataSet GetGuestsAll()
+        public static DataSet GetGuestsAll()
         {
             DBConn dbconn = new DBConn();
-
             return dbconn.GetDataSet("GetGuests_All", null);
         }
 
-        public DataSet GetGuestsSearch(string search)
+        public static DataSet GetGuestsSearch(string input)
         {
-            DBConn dbconn = new DBConn();
             List<DbParameter> parameters = new List<DbParameter>();
-            parameters.Add(new MySqlParameter("Search", "%" + search + "%"));
+            parameters.Add(new MySqlParameter("Search", "%" + input + "%"));
 
+            DBConn dbconn = new DBConn();
             return dbconn.GetDataSet("GetGuests_Search", parameters);
         }
 
-        public void AddGuest(string firstname, string lastname, string address, string city, string postcode, string telephone)
+        public static List<string> GetGuestData(int guestid)
         {
-            DBConn dbconn = new DBConn();
             List<DbParameter> parameters = new List<DbParameter>();
-            parameters.Add(new MySqlParameter("Firstname", firstname));
-            parameters.Add(new MySqlParameter("Lastname", lastname));
-            parameters.Add(new MySqlParameter("Address", address));
-            parameters.Add(new MySqlParameter("City", city));
-            parameters.Add(new MySqlParameter("Postcode", postcode));
-            parameters.Add(new MySqlParameter("Telephone", telephone));
+            parameters.Add(new MySqlParameter("gid", guestid));
 
-
-            dbconn.Execute("AddGuest", parameters);
-        }
-
-        public void EditGuest(int guestid, string firstname, string lastname, string address, string city, string postcode, string telephone)
-        {
             DBConn dbconn = new DBConn();
-            List<DbParameter> parameters = new List<DbParameter>();
-            parameters.Add(new MySqlParameter("Guestid", guestid));
-            parameters.Add(new MySqlParameter("Firstname", firstname));
-            parameters.Add(new MySqlParameter("Lastname", lastname));
-            parameters.Add(new MySqlParameter("Address", address));
-            parameters.Add(new MySqlParameter("City", city));
-            parameters.Add(new MySqlParameter("Postcode", postcode));
-            parameters.Add(new MySqlParameter("Telephone", telephone));
-
-
-            dbconn.Execute("EditGuest", parameters);
-        }
-
-        public List<string>[] GetGuestData(int guestid)
-        {
-            DBConn dbconn = new DBConn();
-            List<DbParameter> parameters = new List<DbParameter>();
-            parameters.Add(new MySqlParameter("Guestid", guestid));
-
             return dbconn.Reader("GetGuestData", parameters);
         }
+
 
         //
         // Folio data
         //
-        public DataSet GetFoliosAll(int isActive)
+        public static DataSet GetFoliosAll(int isActive)
         {
-            DBConn dbconn = new DBConn();
             List<DbParameter> parameters = new List<DbParameter>();
-            parameters.Add(new MySqlParameter("isActive", isActive));
+            parameters.Add(new MySqlParameter("active", isActive));
 
+            DBConn dbconn = new DBConn();
             return dbconn.GetDataSet("GetFolios_Active", parameters);
         }
 
-        public DataSet GetFoliosSearch(string search)
+        public static DataSet GetFoliosSearch(string input)
         {
-            DBConn dbconn = new DBConn();
             List<DbParameter> parameters = new List<DbParameter>();
-            parameters.Add(new MySqlParameter("Search", search));
+            parameters.Add(new MySqlParameter("Search", "%" + input + "%"));
 
+            DBConn dbconn = new DBConn();
             return dbconn.GetDataSet("GetFolios_Search", parameters);
         }
     }
