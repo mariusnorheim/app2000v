7ce3d98dcb96526290aa8a2944113792d9395c53
Marius Norheim <xeroxmf@gmail.com>
2018-05-11 06:56:16 +0200

    Added Housekeeping section

---
+++ HMS/Housekeeping.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace HMS
+{
+    partial class Housekeeping
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.buttonDeleteHousekeeping = new System.Windows.Forms.Button();
+            this.buttonSearchHousekeeping = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonEditHousekeeping = new System.Windows.Forms.Button();
+            this.buttonNewHousekeeping = new System.Windows.Forms.Button();
+            this.dataGridViewHousekeeping = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHousekeeping)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonDeleteHousekeeping
+            // 
+            this.buttonDeleteHousekeeping.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonDeleteHousekeeping.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonDeleteHousekeeping.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDeleteHousekeeping.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDeleteHousekeeping.Location = new System.Drawing.Point(185, 605);
+            this.buttonDeleteHousekeeping.Name = "buttonDeleteHousekeeping";
+            this.buttonDeleteHousekeeping.Size = new System.Drawing.Size(80, 30);
+            this.buttonDeleteHousekeeping.TabIndex = 62;
+            this.buttonDeleteHousekeeping.Text = "Completed";
+            this.buttonDeleteHousekeeping.UseVisualStyleBackColor = false;
+            this.buttonDeleteHousekeeping.Click += new System.EventHandler(this.buttonDeleteHousekeeping_Click);
+            // 
+            // buttonSearchHousekeeping
+            // 
+            this.buttonSearchHousekeeping.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonSearchHousekeeping.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonSearchHousekeeping.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearchHousekeeping.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSearchHousekeeping.Location = new System.Drawing.Point(925, 605);
+            this.buttonSearchHousekeeping.Name = "buttonSearchHousekeeping";
+            this.buttonSearchHousekeeping.Size = new System.Drawing.Size(60, 30);
+            this.buttonSearchHousekeeping.TabIndex = 61;
+            this.buttonSearchHousekeeping.Text = "Search";
+            this.buttonSearchHousekeeping.UseVisualStyleBackColor = false;
+            this.buttonSearchHousekeeping.Click += new System.EventHandler(this.buttonSearchHousekeeping_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.AcceptsTab = true;
+            this.textBoxSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.textBoxSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxSearch.Font = new System.Drawing.Font("Bahnschrift Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSearch.ForeColor = System.Drawing.Color.White;
+            this.textBoxSearch.Location = new System.Drawing.Point(620, 605);
+            this.textBoxSearch.Margin = new System.Windows.Forms.Padding(0);
+            this.textBoxSearch.MaxLength = 80;
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(300, 30);
+            this.textBoxSearch.TabIndex = 60;
+            // 
+            // buttonEditHousekeeping
+            // 
+            this.buttonEditHousekeeping.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonEditHousekeeping.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonEditHousekeeping.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEditHousekeeping.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEditHousekeeping.Location = new System.Drawing.Point(100, 605);
+            this.buttonEditHousekeeping.Name = "buttonEditHousekeeping";
+            this.buttonEditHousekeeping.Size = new System.Drawing.Size(80, 30);
+            this.buttonEditHousekeeping.TabIndex = 59;
+            this.buttonEditHousekeeping.Text = "Edit";
+            this.buttonEditHousekeeping.UseVisualStyleBackColor = false;
+            this.buttonEditHousekeeping.Click += new System.EventHandler(this.buttonEditHousekeeping_Click);
+            // 
+            // buttonNewHousekeeping
+            // 
+            this.buttonNewHousekeeping.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonNewHousekeeping.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonNewHousekeeping.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNewHousekeeping.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNewHousekeeping.Location = new System.Drawing.Point(15, 605);
+            this.buttonNewHousekeeping.Name = "buttonNewHousekeeping";
+            this.buttonNewHousekeeping.Size = new System.Drawing.Size(80, 30);
+            this.buttonNewHousekeeping.TabIndex = 58;
+            this.buttonNewHousekeeping.Text = "New";
+            this.buttonNewHousekeeping.UseVisualStyleBackColor = false;
+            this.buttonNewHousekeeping.Click += new System.EventHandler(this.buttonNewHousekeeping_Click);
+            // 
+            // dataGridViewHousekeeping
+            // 
+            this.dataGridViewHousekeeping.AllowUserToAddRows = false;
+            this.dataGridViewHousekeeping.AllowUserToDeleteRows = false;
+            this.dataGridViewHousekeeping.AllowUserToResizeColumns = false;
+            this.dataGridViewHousekeeping.AllowUserToResizeRows = false;
+            this.dataGridViewHousekeeping.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewHousekeeping.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.dataGridViewHousekeeping.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewHousekeeping.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewHousekeeping.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewHousekeeping.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewHousekeeping.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridViewHousekeeping.GridColor = System.Drawing.Color.DimGray;
+            this.dataGridViewHousekeeping.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dataGridViewHousekeeping.Location = new System.Drawing.Point(15, 50);
+            this.dataGridViewHousekeeping.Name = "dataGridViewHousekeeping";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewHousekeeping.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridViewHousekeeping.RowHeadersVisible = false;
+            this.dataGridViewHousekeeping.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridViewHousekeeping.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewHousekeeping.Size = new System.Drawing.Size(970, 550);
+            this.dataGridViewHousekeeping.TabIndex = 57;
+            this.dataGridViewHousekeeping.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewHousekeeping_CellDoubleClick);
+            // 
+            // Housekeeping
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(1000, 650);
+            this.Controls.Add(this.buttonDeleteHousekeeping);
+            this.Controls.Add(this.buttonSearchHousekeeping);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.buttonEditHousekeeping);
+            this.Controls.Add(this.buttonNewHousekeeping);
+            this.Controls.Add(this.dataGridViewHousekeeping);
+            this.Name = "Housekeeping";
+            this.Load += new System.EventHandler(this.Housekeeping_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHousekeeping)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDeleteHousekeeping;
+        private System.Windows.Forms.Button buttonSearchHousekeeping;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonEditHousekeeping;
+        private System.Windows.Forms.Button buttonNewHousekeeping;
+        public System.Windows.Forms.DataGridView dataGridViewHousekeeping;
+    }
+}
---
+++ HMS/Housekeeping.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HMS
+{
+    public partial class Housekeeping : HMS.Content
+    {
+        public Housekeeping()
+        {
+            InitializeComponent();
+        }
+
+        private void Housekeeping_Load(object sender, EventArgs e)
+        {
+            LoadDataHousekeeping();
+            this.AcceptButton = buttonSearchHousekeeping;
+        }
+
+        // Load dataGridViewTodo
+        public void LoadDataGridViewHousekeeping()
+        {
+            // Hide ID and make readable table headers
+            dataGridViewHousekeeping.Columns[0].Visible = false;
+            dataGridViewHousekeeping.Columns[1].HeaderText = "Room";
+            dataGridViewHousekeeping.Columns[2].HeaderText = "Code";
+            dataGridViewHousekeeping.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewHousekeeping.Columns[3].HeaderText = "Remark";
+        }
+
+        // Load dataset to datagridview
+        public void LoadDataHousekeeping()
+        {
+            // Fetch dataset
+            DataSet housekeepingDS = DBGetData.GetHousekeepingDGVActive();
+
+            if(housekeepingDS != null)
+            {
+                // No tables fetched
+                if(housekeepingDS.Tables.Count == 0)
+                {
+                    new StatusMessage("No datatable found, contact administrator.");
+                    return;
+                }
+                // No rows fetched
+                else if(housekeepingDS.Tables[0].Rows.Count == 0)
+                {
+                    new StatusMessage("No datarows found in housekeeping.");
+                    return;
+                }
+                // Clear labelStatus for previous messages
+                else
+                {
+                    new StatusMessage("");
+                }
+
+                // Set the dataset as source for datagridview and make sure its displayed
+                dataGridViewHousekeeping.DataSource = housekeepingDS;
+                dataGridViewHousekeeping.DataMember = "Get_Housekeeping_Active";
+                LoadDataGridViewHousekeeping();
+            }
+        }
+
+        // TextBox 'Search' click event, empty placeholder text
+        private void textBoxSearch_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.textBoxSearch.Text = "";
+        }
+
+        // DataGridViewHousekeeping double click
+        // Open specialized edit form
+        private void dataGridViewHousekeeping_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // TODO: Edit form
+        }
+
+        // Button 'New'
+        private void buttonNewHousekeeping_Click(object sender, EventArgs e)
+        {
+            // TODO: New form
+        }
+
+        // Button 'Edit'
+        private void buttonEditHousekeeping_Click(object sender, EventArgs e)
+        {
+            // TODO: Edit form
+        }
+
+        // Button 'Completed'
+        private void buttonDeleteHousekeeping_Click(object sender, EventArgs e)
+        {
+            // TODO: Mark post with inactive = 0
+        }
+
+        // Button 'Search'
+        private void buttonSearchHousekeeping_Click(object sender, EventArgs e)
+        {
+            string searchinput = @textBoxSearch.Text.Trim();
+
+            // Fetch dataset
+            DataSet housekeepingDS = DBGetData.GetHousekeepingDGVSearch(searchinput);
+
+            if(housekeepingDS != null)
+            {
+                // No tables fetched
+                if(housekeepingDS.Tables.Count == 0)
+                {
+                    new StatusMessage("No datatable found, contact administrator.");
+                    return;
+                }
+                // No rows fetched
+                else if(housekeepingDS.Tables[0].Rows.Count == 0)
+                {
+                    new StatusMessage("No datarows found matching search.");
+                    return;
+                }
+                // Clear labelStatus for previous messages
+                else
+                {
+                    new StatusMessage("");
+                }
+
+                // Set the dataset as source for datagridview and make sure its displayed
+                dataGridViewHousekeeping.DataSource = housekeepingDS;
+                dataGridViewHousekeeping.DataMember = "Get_Housekeeping_Search";
+                LoadDataGridViewHousekeeping();
+            }
+        }
+    }
+}
