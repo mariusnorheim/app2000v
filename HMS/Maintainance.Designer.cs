97d4f1dcb6a647fc9c7fd3885c75d378718aab8b
Marius Norheim <xeroxmf@gmail.com>
2018-05-11 06:56:28 +0200

    Added Maintainance section

---
+++ HMS/Maintainance.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace HMS
+{
+    partial class Maintainance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.buttonDeleteMaintainance = new System.Windows.Forms.Button();
+            this.buttonSearchMaintainance = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonEditMaintainance = new System.Windows.Forms.Button();
+            this.buttonNewMaintainance = new System.Windows.Forms.Button();
+            this.dataGridViewMaintainance = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMaintainance)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonDeleteMaintainance
+            // 
+            this.buttonDeleteMaintainance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonDeleteMaintainance.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonDeleteMaintainance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDeleteMaintainance.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDeleteMaintainance.Location = new System.Drawing.Point(185, 605);
+            this.buttonDeleteMaintainance.Name = "buttonDeleteMaintainance";
+            this.buttonDeleteMaintainance.Size = new System.Drawing.Size(80, 30);
+            this.buttonDeleteMaintainance.TabIndex = 62;
+            this.buttonDeleteMaintainance.Text = "Completed";
+            this.buttonDeleteMaintainance.UseVisualStyleBackColor = false;
+            this.buttonDeleteMaintainance.Click += new System.EventHandler(this.buttonDeleteMaintainance_Click);
+            // 
+            // buttonSearchMaintainance
+            // 
+            this.buttonSearchMaintainance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonSearchMaintainance.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonSearchMaintainance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearchMaintainance.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSearchMaintainance.Location = new System.Drawing.Point(925, 605);
+            this.buttonSearchMaintainance.Name = "buttonSearchMaintainance";
+            this.buttonSearchMaintainance.Size = new System.Drawing.Size(60, 30);
+            this.buttonSearchMaintainance.TabIndex = 61;
+            this.buttonSearchMaintainance.Text = "Search";
+            this.buttonSearchMaintainance.UseVisualStyleBackColor = false;
+            this.buttonSearchMaintainance.Click += new System.EventHandler(this.buttonSearchMaintainance_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.AcceptsTab = true;
+            this.textBoxSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.textBoxSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxSearch.Font = new System.Drawing.Font("Bahnschrift Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSearch.ForeColor = System.Drawing.Color.White;
+            this.textBoxSearch.Location = new System.Drawing.Point(620, 605);
+            this.textBoxSearch.Margin = new System.Windows.Forms.Padding(0);
+            this.textBoxSearch.MaxLength = 80;
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(300, 30);
+            this.textBoxSearch.TabIndex = 60;
+            // 
+            // buttonEditMaintainance
+            // 
+            this.buttonEditMaintainance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonEditMaintainance.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonEditMaintainance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEditMaintainance.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEditMaintainance.Location = new System.Drawing.Point(100, 605);
+            this.buttonEditMaintainance.Name = "buttonEditMaintainance";
+            this.buttonEditMaintainance.Size = new System.Drawing.Size(80, 30);
+            this.buttonEditMaintainance.TabIndex = 59;
+            this.buttonEditMaintainance.Text = "Edit";
+            this.buttonEditMaintainance.UseVisualStyleBackColor = false;
+            this.buttonEditMaintainance.Click += new System.EventHandler(this.buttonEditMaintainance_Click);
+            // 
+            // buttonNewMaintainance
+            // 
+            this.buttonNewMaintainance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonNewMaintainance.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonNewMaintainance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNewMaintainance.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNewMaintainance.Location = new System.Drawing.Point(15, 605);
+            this.buttonNewMaintainance.Name = "buttonNewMaintainance";
+            this.buttonNewMaintainance.Size = new System.Drawing.Size(80, 30);
+            this.buttonNewMaintainance.TabIndex = 58;
+            this.buttonNewMaintainance.Text = "New";
+            this.buttonNewMaintainance.UseVisualStyleBackColor = false;
+            this.buttonNewMaintainance.Click += new System.EventHandler(this.buttonNewMaintainance_Click);
+            // 
+            // dataGridViewMaintainance
+            // 
+            this.dataGridViewMaintainance.AllowUserToAddRows = false;
+            this.dataGridViewMaintainance.AllowUserToDeleteRows = false;
+            this.dataGridViewMaintainance.AllowUserToResizeColumns = false;
+            this.dataGridViewMaintainance.AllowUserToResizeRows = false;
+            this.dataGridViewMaintainance.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewMaintainance.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.dataGridViewMaintainance.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewMaintainance.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle10;
+            this.dataGridViewMaintainance.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            dataGridViewCellStyle11.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle11.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle11.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle11.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle11.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewMaintainance.DefaultCellStyle = dataGridViewCellStyle11;
+            this.dataGridViewMaintainance.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridViewMaintainance.GridColor = System.Drawing.Color.DimGray;
+            this.dataGridViewMaintainance.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dataGridViewMaintainance.Location = new System.Drawing.Point(15, 50);
+            this.dataGridViewMaintainance.Name = "dataGridViewMaintainance";
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle12.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle12.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle12.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle12.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewMaintainance.RowHeadersDefaultCellStyle = dataGridViewCellStyle12;
+            this.dataGridViewMaintainance.RowHeadersVisible = false;
+            this.dataGridViewMaintainance.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridViewMaintainance.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewMaintainance.Size = new System.Drawing.Size(970, 550);
+            this.dataGridViewMaintainance.TabIndex = 57;
+            this.dataGridViewMaintainance.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewMaintainance_CellDoubleClick);
+            // 
+            // Maintainance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(1000, 650);
+            this.Controls.Add(this.buttonDeleteMaintainance);
+            this.Controls.Add(this.buttonSearchMaintainance);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.buttonEditMaintainance);
+            this.Controls.Add(this.buttonNewMaintainance);
+            this.Controls.Add(this.dataGridViewMaintainance);
+            this.Name = "Maintainance";
+            this.Load += new System.EventHandler(this.Maintainance_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMaintainance)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDeleteMaintainance;
+        private System.Windows.Forms.Button buttonSearchMaintainance;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonEditMaintainance;
+        private System.Windows.Forms.Button buttonNewMaintainance;
+        public System.Windows.Forms.DataGridView dataGridViewMaintainance;
+    }
+}
---
+++ HMS/Maintainance.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HMS
+{
+    public partial class Maintainance : HMS.Content
+    {
+        public Maintainance()
+        {
+            InitializeComponent();
+        }
+
+        private void Maintainance_Load(object sender, EventArgs e)
+        {
+            LoadDataMaintainance();
+            this.AcceptButton = buttonSearchMaintainance;
+        }
+
+        // Load dataGridViewTodo
+        public void LoadDataGridViewMaintainance()
+        {
+            // Hide ID and make readable table headers
+            dataGridViewMaintainance.Columns[0].Visible = false;
+            dataGridViewMaintainance.Columns[1].HeaderText = "Location";
+            dataGridViewMaintainance.Columns[2].HeaderText = "Description";
+            dataGridViewMaintainance.Columns[3].HeaderText = "Priority";
+            dataGridViewMaintainance.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewMaintainance.Columns[4].HeaderText = "Created date";
+            dataGridViewMaintainance.Columns[5].HeaderText = "Created by";
+            dataGridViewMaintainance.Columns[5].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+        }
+
+        // Load dataset to datagridview
+        public void LoadDataMaintainance()
+        {
+            // Fetch dataset
+            DataSet maintainanceDS = DBGetData.GetMaintainanceDGVActive();
+
+            if(maintainanceDS != null)
+            {
+                // No tables fetched
+                if(maintainanceDS.Tables.Count == 0)
+                {
+                    new StatusMessage("No datatable found, contact administrator.");
+                    return;
+                }
+                // No rows fetched
+                else if(maintainanceDS.Tables[0].Rows.Count == 0)
+                {
+                    new StatusMessage("No datarows found in maintainance.");
+                    return;
+                }
+                // Clear labelStatus for previous messages
+                else
+                {
+                    new StatusMessage("");
+                }
+
+                // Set the dataset as source for datagridview and make sure its displayed
+                dataGridViewMaintainance.DataSource = maintainanceDS;
+                dataGridViewMaintainance.DataMember = "Get_Maintainance_Active";
+                LoadDataGridViewMaintainance();
+            }
+        }
+
+        // TextBox 'Search' click event, empty placeholder text
+        private void textBoxSearch_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.textBoxSearch.Text = "";
+        }
+
+        // DataGridViewMaintainance double click
+        // Open specialized edit form
+        private void dataGridViewMaintainance_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // TODO: Edit form
+        }
+
+        // Button 'New'
+        private void buttonNewMaintainance_Click(object sender, EventArgs e)
+        {
+            // TODO: New form
+        }
+
+        // Button 'Edit'
+        private void buttonEditMaintainance_Click(object sender, EventArgs e)
+        {
+            // TODO: Edit form
+        }
+
+        // Button 'Delete'
+        private void buttonDeleteMaintainance_Click(object sender, EventArgs e)
+        {
+            // TODO: Mark post with isactive = 0
+        }
+
+        private void buttonSearchMaintainance_Click(object sender, EventArgs e)
+        {
+            string searchinput = @textBoxSearch.Text.Trim();
+
+            // Fetch dataset
+            DataSet maintainanceDS = DBGetData.GetMaintainanceDGVSearch(searchinput);
+
+            if(maintainanceDS != null)
+            {
+                // No tables fetched
+                if(maintainanceDS.Tables.Count == 0)
+                {
+                    new StatusMessage("No datatable found, contact administrator.");
+                    return;
+                }
+                // No rows fetched
+                else if(maintainanceDS.Tables[0].Rows.Count == 0)
+                {
+                    new StatusMessage("No datarows found matching search.");
+                    return;
+                }
+                // Clear labelStatus for previous messages
+                else
+                {
+                    new StatusMessage("");
+                }
+
+                // Set the dataset as source for datagridview and make sure its displayed
+                dataGridViewMaintainance.DataSource = maintainanceDS;
+                dataGridViewMaintainance.DataMember = "Get_Maintainance_Search";
+                LoadDataGridViewMaintainance();
+            }
+        }
+    }
+}
