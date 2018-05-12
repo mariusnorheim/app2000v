488c5e4cdb61c79b6062d9b63fe83e71be06d5f7
Marius Norheim <xeroxmf@gmail.com>
2018-05-12 11:29:53 +0200

    Added Payment section

---
+++ HMS/Payment.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace HMS
+{
+    partial class Payment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle22 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle23 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle24 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.buttonDeletePaymentItem = new System.Windows.Forms.Button();
+            this.buttonSearchPaymentItem = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonEditPaymentItem = new System.Windows.Forms.Button();
+            this.buttonNewPaymentItem = new System.Windows.Forms.Button();
+            this.dataGridViewPaymentItem = new System.Windows.Forms.DataGridView();
+            this.buttonSubMenu2 = new System.Windows.Forms.Button();
+            this.buttonSubMenu1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPaymentItem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonDeletePaymentItem
+            // 
+            this.buttonDeletePaymentItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonDeletePaymentItem.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonDeletePaymentItem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDeletePaymentItem.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDeletePaymentItem.Location = new System.Drawing.Point(185, 605);
+            this.buttonDeletePaymentItem.Name = "buttonDeletePaymentItem";
+            this.buttonDeletePaymentItem.Size = new System.Drawing.Size(80, 30);
+            this.buttonDeletePaymentItem.TabIndex = 62;
+            this.buttonDeletePaymentItem.Text = "Delete";
+            this.buttonDeletePaymentItem.UseVisualStyleBackColor = false;
+            this.buttonDeletePaymentItem.Click += new System.EventHandler(this.buttonDeletePaymentItem_Click);
+            // 
+            // buttonSearchPaymentItem
+            // 
+            this.buttonSearchPaymentItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonSearchPaymentItem.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonSearchPaymentItem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearchPaymentItem.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSearchPaymentItem.Location = new System.Drawing.Point(925, 605);
+            this.buttonSearchPaymentItem.Name = "buttonSearchPaymentItem";
+            this.buttonSearchPaymentItem.Size = new System.Drawing.Size(60, 30);
+            this.buttonSearchPaymentItem.TabIndex = 61;
+            this.buttonSearchPaymentItem.Text = "Search";
+            this.buttonSearchPaymentItem.UseVisualStyleBackColor = false;
+            this.buttonSearchPaymentItem.Click += new System.EventHandler(this.buttonSearchPaymentItem_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.AcceptsTab = true;
+            this.textBoxSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.textBoxSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxSearch.Font = new System.Drawing.Font("Bahnschrift Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSearch.ForeColor = System.Drawing.Color.White;
+            this.textBoxSearch.Location = new System.Drawing.Point(620, 605);
+            this.textBoxSearch.Margin = new System.Windows.Forms.Padding(0);
+            this.textBoxSearch.MaxLength = 80;
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(300, 30);
+            this.textBoxSearch.TabIndex = 60;
+            // 
+            // buttonEditPaymentItem
+            // 
+            this.buttonEditPaymentItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonEditPaymentItem.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonEditPaymentItem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEditPaymentItem.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEditPaymentItem.Location = new System.Drawing.Point(100, 605);
+            this.buttonEditPaymentItem.Name = "buttonEditPaymentItem";
+            this.buttonEditPaymentItem.Size = new System.Drawing.Size(80, 30);
+            this.buttonEditPaymentItem.TabIndex = 59;
+            this.buttonEditPaymentItem.Text = "Edit";
+            this.buttonEditPaymentItem.UseVisualStyleBackColor = false;
+            this.buttonEditPaymentItem.Click += new System.EventHandler(this.buttonEditPaymentItem_Click);
+            // 
+            // buttonNewPaymentItem
+            // 
+            this.buttonNewPaymentItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.buttonNewPaymentItem.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.buttonNewPaymentItem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNewPaymentItem.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNewPaymentItem.Location = new System.Drawing.Point(15, 605);
+            this.buttonNewPaymentItem.Name = "buttonNewPaymentItem";
+            this.buttonNewPaymentItem.Size = new System.Drawing.Size(80, 30);
+            this.buttonNewPaymentItem.TabIndex = 58;
+            this.buttonNewPaymentItem.Text = "New";
+            this.buttonNewPaymentItem.UseVisualStyleBackColor = false;
+            this.buttonNewPaymentItem.Click += new System.EventHandler(this.buttonNewPaymentItem_Click);
+            // 
+            // dataGridViewPaymentItem
+            // 
+            this.dataGridViewPaymentItem.AllowUserToAddRows = false;
+            this.dataGridViewPaymentItem.AllowUserToDeleteRows = false;
+            this.dataGridViewPaymentItem.AllowUserToResizeColumns = false;
+            this.dataGridViewPaymentItem.AllowUserToResizeRows = false;
+            this.dataGridViewPaymentItem.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewPaymentItem.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.dataGridViewPaymentItem.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle22.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle22.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle22.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle22.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle22.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle22.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle22.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewPaymentItem.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle22;
+            this.dataGridViewPaymentItem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle23.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle23.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            dataGridViewCellStyle23.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle23.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle23.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle23.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle23.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewPaymentItem.DefaultCellStyle = dataGridViewCellStyle23;
+            this.dataGridViewPaymentItem.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridViewPaymentItem.GridColor = System.Drawing.Color.DimGray;
+            this.dataGridViewPaymentItem.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dataGridViewPaymentItem.Location = new System.Drawing.Point(15, 50);
+            this.dataGridViewPaymentItem.Name = "dataGridViewPaymentItem";
+            dataGridViewCellStyle24.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle24.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle24.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle24.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle24.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle24.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle24.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewPaymentItem.RowHeadersDefaultCellStyle = dataGridViewCellStyle24;
+            this.dataGridViewPaymentItem.RowHeadersVisible = false;
+            this.dataGridViewPaymentItem.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridViewPaymentItem.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewPaymentItem.Size = new System.Drawing.Size(970, 550);
+            this.dataGridViewPaymentItem.TabIndex = 57;
+            this.dataGridViewPaymentItem.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewPaymentItem_CellDoubleClick);
+            // 
+            // buttonSubMenu2
+            // 
+            this.buttonSubMenu2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(84)))), ((int)(((byte)(126)))));
+            this.buttonSubMenu2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSubMenu2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.buttonSubMenu2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSubMenu2.Font = new System.Drawing.Font("Ubuntu", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSubMenu2.Location = new System.Drawing.Point(505, 10);
+            this.buttonSubMenu2.Name = "buttonSubMenu2";
+            this.buttonSubMenu2.Size = new System.Drawing.Size(105, 30);
+            this.buttonSubMenu2.TabIndex = 64;
+            this.buttonSubMenu2.Text = "Category";
+            this.buttonSubMenu2.UseVisualStyleBackColor = false;
+            this.buttonSubMenu2.Click += new System.EventHandler(this.buttonSubMenu2_Click);
+            // 
+            // buttonSubMenu1
+            // 
+            this.buttonSubMenu1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(84)))), ((int)(((byte)(126)))));
+            this.buttonSubMenu1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSubMenu1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.buttonSubMenu1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSubMenu1.Font = new System.Drawing.Font("Ubuntu", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSubMenu1.Location = new System.Drawing.Point(395, 10);
+            this.buttonSubMenu1.Name = "buttonSubMenu1";
+            this.buttonSubMenu1.Size = new System.Drawing.Size(105, 30);
+            this.buttonSubMenu1.TabIndex = 63;
+            this.buttonSubMenu1.Text = "Item";
+            this.buttonSubMenu1.UseVisualStyleBackColor = false;
+            this.buttonSubMenu1.Click += new System.EventHandler(this.buttonSubMenu1_Click);
+            // 
+            // Payment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(1000, 650);
+            this.Controls.Add(this.buttonSubMenu2);
+            this.Controls.Add(this.buttonSubMenu1);
+            this.Controls.Add(this.buttonDeletePaymentItem);
+            this.Controls.Add(this.buttonSearchPaymentItem);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.buttonEditPaymentItem);
+            this.Controls.Add(this.buttonNewPaymentItem);
+            this.Controls.Add(this.dataGridViewPaymentItem);
+            this.Name = "Payment";
+            this.Load += new System.EventHandler(this.Payment_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPaymentItem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDeletePaymentItem;
+        private System.Windows.Forms.Button buttonSearchPaymentItem;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonEditPaymentItem;
+        private System.Windows.Forms.Button buttonNewPaymentItem;
+        public System.Windows.Forms.DataGridView dataGridViewPaymentItem;
+        private System.Windows.Forms.Button buttonSubMenu2;
+        private System.Windows.Forms.Button buttonSubMenu1;
+    }
+}
---
+++ HMS/Payment.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HMS
+{
+    public partial class Payment : HMS.Content
+    {
+        public Payment()
+        {
+            InitializeComponent();
+        }
+
+        private void Payment_Load(object sender, EventArgs e)
+        {
+            LoadDataPaymentItem();
+            this.AcceptButton = buttonSearchPaymentItem;
+        }
+
+        // Load dataGridViePaymentItem
+        public void LoadDataGridViewPaymentItem()
+        {
+            // Hide ID and make readable table headers
+            dataGridViewPaymentItem.Columns[0].Visible = false;
+            dataGridViewPaymentItem.Columns[1].HeaderText = "Item name";
+            dataGridViewPaymentItem.Columns[2].HeaderText = "Cost";
+            dataGridViewPaymentItem.Columns[3].HeaderText = "Category";
+            dataGridViewPaymentItem.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+        }
+
+        // Load dataset to datagridview
+        public void LoadDataPaymentItem()
+        {
+            // Fetch dataset
+            DataSet paymentitemDS = DBGetData.GetPaymentItemDGVAll();
+
+            if(paymentitemDS != null)
+            {
+                // No tables fetched
+                if(paymentitemDS.Tables.Count == 0)
+                {
+                    new StatusMessage("No datatable found, contact administrator.");
+                    return;
+                }
+                // No rows fetched
+                else if(paymentitemDS.Tables[0].Rows.Count == 0)
+                {
+                    new StatusMessage("No datarows found in billing_item.");
+                    return;
+                }
+                // Clear labelStatus for previous messages
+                else
+                {
+                    new StatusMessage("");
+                }
+
+                // Set the dataset as source for datagridview and make sure its displayed
+                dataGridViewPaymentItem.DataSource = paymentitemDS;
+                dataGridViewPaymentItem.DataMember = "Get_PaymentItem_All";
+                LoadDataGridViewPaymentItem();
+            }
+        }
+
+        // Button 'Item'
+        private void buttonSubMenu1_Click(object sender, EventArgs e)
+        {
+            LoadDataPaymentItem();
+            this.AcceptButton = buttonSearchPaymentItem;
+        }
+
+        // Button 'Category'
+        private void buttonSubMenu2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //
+        // Item section
+        //
+
+        // DataGridViewPaymentItem double click
+        // Open specialized edit form
+        private void dataGridViewPaymentItem_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // TODO: Edit form
+        }
+
+        // Button 'New'
+        private void buttonNewPaymentItem_Click(object sender, EventArgs e)
+        {
+            // TODO: New form
+        }
+
+        // Button 'Edit'
+        private void buttonEditPaymentItem_Click(object sender, EventArgs e)
+        {
+            // TODO: Edit form
+        }
+
+        // Button 'Delete'
+        private void buttonDeletePaymentItem_Click(object sender, EventArgs e)
+        {
+            // TODO: Delete item
+        }
+
+        // Button 'Search'
+        private void buttonSearchPaymentItem_Click(object sender, EventArgs e)
+        {
+            string searchinput = @textBoxSearch.Text.Trim();
+
+            // Fetch dataset
+            DataSet paymentitemDS = DBGetData.GetPaymentItemDGVSearch(searchinput);
+
+            if(paymentitemDS != null)
+            {
+                // No tables fetched
+                if(paymentitemDS.Tables.Count == 0)
+                {
+                    new StatusMessage("No datatable found, contact administrator.");
+                    return;
+                }
+                // No rows fetched
+                else if(paymentitemDS.Tables[0].Rows.Count == 0)
+                {
+                    new StatusMessage("No datarows found matching search.");
+                    return;
+                }
+                // Clear labelStatus for previous messages
+                else
+                {
+                    new StatusMessage("");
+                }
+
+                // Set the dataset as source for datagridview and make sure its displayed
+                dataGridViewPaymentItem.DataSource = paymentitemDS;
+                dataGridViewPaymentItem.DataMember = "Get_PaymentItem_Search";
+                LoadDataGridViewPaymentItem();
+            }
+        }
+
+
+        //
+        // Category section
+        //
+    }
+}
